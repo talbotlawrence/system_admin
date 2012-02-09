1677c2071f87e073f00180bf9affcd23d8d75287
talbot <talbotlawrence75@gmail.com>
2012-02-09 17:13:24 -0600

    added fileutils

--- files2proc.rb
+++ files2proc.rb
@@ -1,28 +1,24 @@
-#connect to the remote server
-#cd in to the proper directory--------------------
-#if any files exist in the directory----------------
-#store existing files into an array-----------------
-#loop through each of them
-#determine how long each file has been sitting in the directory
-#if longer than five minutes
-#then send an email to talbotlawrence@eftsource.com
-#puts "All clear!!"
-#puts "These files are old: #{file_list}, #{age_of_file}
-
 class AgeOfFiles
+require 'fileutils'
 
-Dir.chdir("/home/talbotlawrence/Desktop/tempin/filestoprocess")
-#file_list = Dir.glob('*')
+Dir.chdir("/home/talbotlawrence/Desktop/tempin/filestoprocess")  
+#point attention at the "filestoprocess" directory (above)
 
-Dir.foreach(".") do |f|
-  next if f == '.' or f == '..'
+Dir.foreach(".") do |f|                                     #step through each file=f       
+  next if f == '.' or f == '..'                             #unwanted directories
   time_now = Time.now
-  original_time = File.mtime(f)
-  age_of_file = ((time_now - original_time) / 60).to_i
-    if age_of_file >= 5 
-      then puts "WARNING: #{f} is #{age_of_file} minutes old---------------------MOVE YOUR ASS!!!!!!"
-    else puts "All clear: #{f} is still young at #{age_of_file} minute(s) old!"
+  original_time = File.mtime(f)                             #modified times of each file
+  age_of_file = ((time_now - original_time) / 60).to_i      #age in minutes, to_i=no decimal
+    if age_of_file >= 5
+      then FileUtils.copy(f, "/home/talbotlawrence/Desktop/tempin/backup"); 
+        FileUtils.move(f, "/home/talbotlawrence/Desktop/tempin");     #drop into production
+          puts "Dropped #{f} back into production and backed it up on /home/talbotlawrence/Desktop/tempin/backup/"
+      else puts "All clear: #{f} is still young at #{age_of_file} minute(s) old!"
+    end
   end
 end
-
-end
+#    if age_of_file >= 5 
+#      then puts "WARNING: #{f} is #{age_of_file} minutes old---------------------MOVE YOUR ASS!!!!!!"
+#    else puts "All clear: #{f} is still young at #{age_of_file} minute(s) old!"
+#  end
+#end
